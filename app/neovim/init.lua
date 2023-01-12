1763cae2364fa90efd5e8683bb165b0d939af12f
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2023-01-12 21:17:02 +0800

    Create init.lua

---
+++ app/neovim/init.lua
@@ -0,0 +1,150 @@
+require('packer').startup(function(use)
+    -- Package manager
+    use 'wbthomason/packer.nvim'
+
+    use { -- Highlight, edit, and navigate code
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+         pcall(require('nvim-treesitter.install').update { with_sync = true })
+        end,
+    }
+
+    use { -- Additional text objects via treesitter
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        after = 'nvim-treesitter',
+    }
+
+    -- Git related plugins
+    use 'navarasu/onedark.nvim' -- Theme inspired by Atom
+    use 'nvim-lualine/lualine.nvim' -- Fancier statusline
+end)
+
+-- [[ Setting options ]]
+-- See `:help vim.o`
+
+-- utf8
+vim.g.encoding = "UTF-8"
+vim.o.fileencoding = "utf-8"
+
+-- 搜索高亮
+vim.o.hlsearch = false
+
+-- 显示行号
+vim.wo.number = true
+
+-- 缩进4个空格等于一个Tab
+vim.o.tabstop = 4
+vim.bo.tabstop = 4
+vim.o.softtabstop = 4 
+vim.o.shiftround = true
+
+-- >> << 时移动长度
+vim.o.shiftwidth = 4
+vim.bo.shiftwidth = 4
+
+-- 空格替代tab
+vim.o.expandtab = true
+vim.bo.expandtab = true
+
+-- 新行对齐当前行
+vim.o.autoindent = true
+vim.bo.autoindent = true
+vim.o.smartindent = true
+
+-- 搜索大小写不敏感，除非包含大写
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- 高亮所在行
+vim.wo.cursorline = true
+
+-- 边输入边搜索
+vim.o.incsearch = true
+
+-- 禁止折行
+vim.wo.wrap = false
+
+-- Set colorscheme
+vim.o.termguicolors = true
+vim.cmd [[colorscheme onedark]]
+
+-- 光标在行首尾时<Left><Right>可以跳到下一行
+vim.o.whichwrap = "<,>,[,]"
+
+-- H,L 跳到行首,行尾
+vim.keymap.set('n', 'H', '^', { silent = true })
+vim.keymap.set('n', 'L', '$', { silent = true })
+
+-- Set lualine as statusline
+-- See `:help lualine.txt`
+require('lualine').setup {
+    options = {
+        icons_enabled = false,
+        theme = 'onedark',
+        component_separators = '|',
+        section_separators = '',
+    },
+}
+
+-- [[ Configure Treesitter ]]
+-- See `:help nvim-treesitter`
+require('nvim-treesitter.configs').setup {
+    -- Add languages to be installed here that you want installed for treesitter
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'vim', 'python', 'json', 'bash' },
+
+    highlight = { enable = true },
+    indent = { enable = true, disable = { 'python' } },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = '<c-space>',
+            node_incremental = '<c-space>',
+            scope_incremental = '<c-s>',
+            node_decremental = '<c-backspace>',
+        },
+    },
+    textobjects = {
+        select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ['aa'] = '@parameter.outer',
+                ['ia'] = '@parameter.inner',
+                ['af'] = '@function.outer',
+                ['if'] = '@function.inner',
+                ['ac'] = '@class.outer',
+                ['ic'] = '@class.inner',
+            },
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                [']m'] = '@function.outer',
+                [']]'] = '@class.outer',
+            },
+            goto_next_end = {
+                [']M'] = '@function.outer',
+                [']['] = '@class.outer',
+            },
+            goto_previous_start = {
+                ['[m'] = '@function.outer',
+                ['[['] = '@class.outer',
+            },
+            goto_previous_end = {
+                ['[M'] = '@function.outer',
+                ['[]'] = '@class.outer',
+            },
+        },
+        swap = {
+            enable = true,
+            swap_next = {
+                ['<leader>a'] = '@parameter.inner',
+            },
+            swap_previous = {
+                ['<leader>A'] = '@parameter.inner',
+            },
+        },
+    },
+}
