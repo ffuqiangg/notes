f5a7804565a8fde01a25ae7cdab7260c9def70b8
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2023-01-20 20:01:42 +0800

    Update init.lua

--- app/neovim/init.lua
+++ app/neovim/init.lua
@@ -1,3 +1,6 @@
+-- Install Packer
+-- Linux: git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+-- Windows Powershell: git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
 require('packer').startup(function(use)
     -- Package manager
     use 'wbthomason/packer.nvim'
