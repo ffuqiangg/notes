ae1118fc81d37ba93d82f0e039f1030cb16ae7f5
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2023-01-25 21:18:16 +0800

    Update init.lua

--- app/neovim/init.lua
+++ app/neovim/init.lua
@@ -110,6 +110,12 @@ vim.g.maplocalleader = ' '
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- Remap for move corsor
+vim.keymap.set('n', '<C-H>', '^', { silent = true })
+vim.keymap.set('n', '<C-L>', '$', { silent = true })
+vim.keymap.set('n', '<C-J>', '<C-D>', { silent = true })
+vim.keymap.set('n', '<C-k>', '<C-U>', { silent = true })
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
